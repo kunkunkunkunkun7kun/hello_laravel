12d1bd00c1cf23523745cf425233f2a02e815ab0
nzero3 <939511779@qq.com>
2018-03-02 07:28:45 +0000

    20180302-1

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Hello Laravel! - by Nzero</div>
+                <div class="title">Hello Laravel! - by Nzero3</div>
             </div>
         </div>
     </body>
